df93d36077793a92b0638e077e8bced82dd0b63b
TiiVa <142997561+TiiVa@users.noreply.github.com>
2026-07-13 12:57:18 +0200

    StockRepository done, needs testing

---
+++ src/StockCareV2.Application/DTOs/Converters/StockConvert.cs
@@ -0,0 +1,47 @@
+﻿using StockCareV2.Domain.Entities;
+
+
+namespace StockCareV2.Application.DTOs.Converters
+{
+    public static class StockConvert
+    {
+        public static StockDto ConvertToDto(this Stock stock)
+        {
+            var d = new StockDto
+            {
+                Id = stock.Id,
+                IsActive = stock.IsActive,
+                SupplierName = stock.SupplierName,
+                ProductsOnStock = stock.ProductsOnStock.Select(x => new OrderProductDto
+                {
+                    ProductId = x.ProductId,
+                    StockId = x.StockId,
+                    OrderSum = x.OrderSum,
+                    Quantity = x.Quantity
+                }).ToList()
+            };
+
+            if (stock.Orders is not null) d.Orders = stock.Orders.Select(x => x.ConvertToDto()).ToList();
+
+
+            return d;
+        }
+
+        public static Stock ConvertToStock(this StockDto d)
+        {
+            var stockModel = new Stock()
+            {
+                IsActive = d.IsActive,
+                SupplierName = d.SupplierName,
+                 
+            };
+
+            if (d.ProductsOnStock is null) stockModel.ProductsOnStock = new List<OrderProduct>();
+            if (d.Orders is null) stockModel.Orders = new List<Order>();
+
+            return stockModel;
+        }
+
+       
+    }
+}
--- src/StockCareV2.Infrastructure/Repositories/StockRepository.cs
+++ src/StockCareV2.Infrastructure/Repositories/StockRepository.cs
@@ -1,37 +1,76 @@
-﻿using StockCareV2.Application.DTOs;
+﻿using Microsoft.EntityFrameworkCore;
+using StockCareV2.Application.DTOs;
+using StockCareV2.Application.DTOs.Converters;
 using StockCareV2.Application.Interfaces.RepositoryInterfaces;
 using StockCareV2.Infrastructure.Data;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace StockCareV2.Infrastructure.Repositories
 {
     public class StockRepository(AppDbContext context) : IStockRepository
     {
-        public Task<bool> AddAsync(StockDto entity)
+
+        public async Task<IEnumerable<StockDto>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            return await context.Stocks
+                .Select(x => x.ConvertToDto()).ToListAsync();
         }
 
-        public Task<bool> DeleteAsync(Guid id)
+        public async Task<StockDto> GetByIdAsync(Guid id)
         {
-            throw new NotImplementedException();
+            var stock = await context.Stocks.FirstOrDefaultAsync(s => s.Id == id);
+
+            if (stock is null) return new StockDto();
+
+            return stock.ConvertToDto();
+
+        }
+        public async Task<bool> AddAsync(StockDto entity)
+        {
+            var stockModel = entity.ConvertToStock();
+
+            if (stockModel is null) return false;
+
+            await context.Stocks.AddAsync(stockModel);
+
+            await context.SaveChangesAsync();
+
+            return true;
         }
 
-        public Task<IEnumerable<StockDto>> GetAllAsync()
+        public async Task<bool> UpdateAsync(StockDto entity, Guid id)
         {
-            throw new NotImplementedException();
+            var stockModel = entity.ConvertToStock();
+            var stockToUpdate = await context.Stocks.FirstOrDefaultAsync(s => s.Id == id);
+
+            if (stockToUpdate is null) return false;
+
+            stockToUpdate.IsActive = stockModel.IsActive;
+            stockToUpdate.SupplierName = stockModel.SupplierName;
+            stockToUpdate.ProductsOnStock = stockModel.ProductsOnStock;
+            stockToUpdate.Orders = stockModel.Orders;
+
+            await context.SaveChangesAsync();
+
+            return true;
+
         }
 
-        public Task<StockDto> GetByIdAsync(Guid id)
+        public async Task<bool> DeleteAsync(Guid id)
         {
-            throw new NotImplementedException();
+            var stockToSoftDelete = await context.Stocks.FirstOrDefaultAsync(s => s.Id == id);
+
+            if (stockToSoftDelete is null) return false;
+
+            var entityEntry = context.Stocks.Update(stockToSoftDelete);
+            entityEntry.Property(s => s.IsActive).CurrentValue = false;
+
+            await context.SaveChangesAsync();
+
+            return true;
         }
 
-        public Task<bool> UpdateAsync(StockDto entity, Guid id)
-        {
-            throw new NotImplementedException();
-        }
+
+
+
     }
 }
